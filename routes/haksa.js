85d76f57f45b3a4d48f1ff5aa812d57133308756
junjunshim <shimdj0425@naver.com>
2026-04-30 12:28:12 +0900

    Feat : oracledb 라이브러리 추가 및 교수 데이터 조회 router 추가

--- routes/haksa.js
+++ routes/haksa.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { getConnection } = require('../connect');
+const oracledb = require('oracledb');
 
 // 학사관리의 교수 라우터
 router.get('/pro', function(req, res, next){
     res.render('index', {title: '교수관리', pageName: 'haksa/professors.ejs'})
+});
+
+// 학사 관리 - 교수 리스트 라우터
+router.get('/pro/list.json', async function(req, res, next){
+    var con;
+    try{
+        con = await getConnection();
+        const sql="select * from professors";
+        const result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+        res.send(result.rows);
+    }catch(err){
+
+    }finally{
+        if(con) await con.close();
+    }
 });
 
 // 학사관리의 학생 라우터
