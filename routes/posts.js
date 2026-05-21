9de77a8bf274348d67fd5b142e822128939285da
junjunshim <shimdj0425@naver.com>
2026-05-21 09:08:49 +0900

    Router : 게시글 경로 추가 및 초기 게시글 추가

--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,9 @@ var router = express.Router();
 const { getConnection } = require('../connect');
 const oracledb = require('oracledb');
 
+/* 게시글 목록 */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: '게시글', pageName: 'posts/list.ejs' });
+});
+
 module.exports = router;
