e26bfe03cb7aee1e0e7a5a32f0f40ad3f9055e92
junjunshim <shimdj0425@naver.com>
2026-05-21 09:17:31 +0900

    Feat : 게시글 목록을 가져오는 로직 추가 및 게시글 내역 추가

--- routes/posts.js
+++ routes/posts.js
@@ -8,4 +8,33 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: '게시글', pageName: 'posts/list.ejs' });
 });
 
+//게시글 목록 데이터
+router.get('/list.json', async function(req, res){
+    const page=parseInt(req.query.page) || 1;
+    const size=parseInt(req.query.size) || 10;
+
+    const startRow = (page-1) * size + 1;
+    const endRow = page * size;
+    
+    var con;
+    
+    try{
+        con = await getConnection();
+        
+        var sql="select * from view_posts where rn between :startRow and :endRow";
+        var result=await con.execute(sql, {startRow, endRow}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+        const list=result.rows;
+        
+        sql="select count(*) from view_posts";
+        result=await con.execute(sql);
+        const total=result.rows[0][0];
+        
+        res.send({list, total});
+    }catch(err){
+        console.log(err);
+    }finally{
+        if(con) await con.close();
+    }
+});
+
 module.exports = router;
